cd3369acb98109647906575abb106dd0adfd6b8c
Carlos <cruiara0501@g.educaand.es>
2024-01-03 13:51:32 +0100

    avance en practica

--- src/main/java/es/practica2/tema2PSP/rest/videoclubRest.java
+++ src/main/java/es/practica2/tema2PSP/rest/videoclubRest.java
@@ -17,7 +17,7 @@ import jakarta.servlet.http.HttpSession;
 @RestController
 public class videoclubRest {
 	//se le pasa archivo json y la sesion para almacenar en sesion
-	@RequestMapping(method = RequestMethod.POST, value = "/movies/upload")
+	@RequestMapping(method = RequestMethod.POST, value = "/movies/upload", consumes = "multipart/form-data")
 	public ResponseEntity<?> uploadMovies(@RequestPart("jsonFile") MultipartFile jsonFile, HttpSession session) throws VideoclubException
 	{
 		try
@@ -35,8 +35,33 @@ public class videoclubRest {
 			throw new VideoclubException(500, error);
 		}
 	}
-	 @RequestMapping(method = RequestMethod.GET, value = "/movies/download")
+	 @RequestMapping(method = RequestMethod.GET, value = "/movies/download", produces = "multipart/form-data")
 	public ResponseEntity<?> downloadMovies(@RequestPart HttpSession session)
+	{
+		String jsonContent = (String)session.getAttribute("jsonContent");
+		return ResponseEntity.ok().body(jsonContent);
+		
+	}
+	@RequestMapping(method = RequestMethod.POST, value = "/users/upload", consumes = "multipart/form-data")
+	public ResponseEntity<?> uploadUsers(@RequestPart("jsonFile") MultipartFile jsonFile, HttpSession session) throws VideoclubException
+	{
+		try
+		{
+			//Lee el json como un string
+			String jsonContent = new String(FileCopyUtils.copyToByteArray(jsonFile.getInputStream()));
+			//Almacenar en sesion
+			session.setAttribute("jsonCOntent", jsonContent);
+			
+			return ResponseEntity.ok("La carga de datos fue realizada correctamente");
+		}
+		catch(IOException exception)
+		{
+			String error = "Error al leer el archivo";
+			throw new VideoclubException(500, error);
+		}
+	}
+	@RequestMapping(method = RequestMethod.GET, value = "/users/download", produces = "multipart/form-data")
+	public ResponseEntity<?> downloadUsers(@RequestPart HttpSession session)
 	{
 		String jsonContent = (String)session.getAttribute("jsonContent");
 		return ResponseEntity.ok().body(jsonContent);
